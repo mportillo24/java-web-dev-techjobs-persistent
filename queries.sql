b7c281dd072e4e0fefc5e948796ae5f87a5affbe
Mario Portillo-Parra <mario.portillo24@gmail.com>
2020-03-18 19:58:42 -0500

    Finished assignment 4.

--- queries.sql
+++ queries.sql
@@ -1,15 +1,18 @@
 ## Part 1: Test it with SQL
-id
-int PK
-employer
-varchar(255)
-name
-varchar(255)
-skills
-varchar(255)
+SELECT * FROM job;
 
 ## Part 2: Test it with SQL
 
+SELECT name FROM employer WHERE location="St Louis";
+
 ## Part 3: Test it with SQL
 
+DROP TABLE techjobs.job;
+
 ## Part 4: Test it with SQL
+
+SELECT name, description
+FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name;
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,15 +1,20 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
-import javax.validation.constraints.Max;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
 
     @NotBlank
-    @Max(255)
+    @Size(min=2, max=255)
     private String description;
+
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs;
 
     public Skill() {
 
